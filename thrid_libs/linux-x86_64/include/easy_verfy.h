bce5c03dcb2817b5a908c03bcf5f3742c05cfd0b
AYssu <2997036064@qq.com>
2026-05-19 17:34:57 +0800

    更新写方法问题

--- thrid_libs/linux-x86_64/include/easy_verfy.h
+++ thrid_libs/linux-x86_64/include/easy_verfy.h
@@ -9,13 +9,12 @@
  #include <obfuscate.h>
  #include <unordered_map>
  #include <any>
-std::string ciphergate_host = OBFUSCATE("http://localhost:8080");// 仅 host 可配置
-std::string ciphergate_app_id = OBFUSCATE("1");
-std::string ciphergate_client_version = OBFUSCATE("1.0.0");
-std::string ciphergate_app_key = OBFUSCATE("V5wXrxIYI0zAi9uUQ7RFWKzod9iojDdy");
-std::string ciphergate_app_secret = OBFUSCATE("D91fujPP0c41ynOvcbqKzgoS66FcSHRrkWKG6L5hiCXp5hXV3dyaHCOD8hpb9WsA");
-std::string ciphergate_aes_key = OBFUSCATE("cg_demo_key_1234");
-
+ std::string ciphergate_host = OBFUSCATE("https://www.ayssu.com");// 仅 host 可配置
+ std::string ciphergate_app_id = OBFUSCATE("1");
+ std::string ciphergate_client_version = OBFUSCATE("1.1.1");
+ std::string ciphergate_app_key = OBFUSCATE("wCmythK0COZ8IEcXbIS4aD7QAswN12121");
+ std::string ciphergate_app_secret = OBFUSCATE("Mqw7Kvrvg9V8ITjfL9ziMm1212121UT08KLNU0Ckz2Ug8aZNNh9jRrkPRgFREkng");
+ std::string ciphergate_aes_key = OBFUSCATE("cg_demo_key_1234");
  std::unordered_map<std::string, std::function<std::any(std::vector<std::any>)>> method_map;
  #endif //SEAV3_EASY_VERFY_H
 
